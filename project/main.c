9ff4f39cfb20d8e7da803f61a47e71f2cc0f75df
HDom98 <hadominguez3@miners.utep.edu>
2020-11-23 16:23:02 -0700

    added string error

--- project/main.c
+++ project/main.c
@@ -3,14 +3,25 @@
 #include "led.h"
 #include "buzzer.h"
 #include "stateMachines.h"
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+
 
 int main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-  switch_init();
-  led_init();
-  buzzer_init();
+  //switch_init();
+  //led_init();
+  //buzzer_init();
   
-  enableWDTInterrupts();	/* enable periodic interrupt */
+  //enableWDTInterrupts();	/* enable periodic interrupt */
+  u_char width = screenWidth, height = screenHeight;
+
+  clearScreen(COLOR_BLACK);
+
+  drawString8x12(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  fillRectangle(5, 40, 8, 50, COLOR_BLUE);
+  fillRectangle(width - 13, 40, 8, 50, COLOR_RED);
 
   or_sr(0x18);		/* CPU off, GIE on */
 }
