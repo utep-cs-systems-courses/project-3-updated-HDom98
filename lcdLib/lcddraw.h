01433ae0655da6d96c482bf55e12aa055754d320
HDom98 <hadominguez3@miners.utep.edu>
2020-11-26 14:24:48 -0700

    added fonts 8x12 and 11x16

--- lcdLib/lcddraw.h
+++ lcdLib/lcddraw.h
@@ -50,6 +50,51 @@ void drawString5x7(u_char col, u_char row, char *string,
 void drawChar5x7(u_char col, u_char row, char c, 
 		 u_int fgColorBGR, u_int bgColorBGR);
 
+
+/** Draw string at col,row
+ *  Type:
+ *  FONT_SM - small (5x8,) FONT_MD - medium (8x12,) FONT_LG - large (11x16)
+ *  FONT_SM_BKG, FONT_MD_BKG, FONT_LG_BKG - as above, but with background color
+ *  Adapted from RobG's EduKit
+ *
+ *  \param col Column to start drawing string
+ *  \param row Row to start drawing string
+ *  \param string The string
+ *  \param fgColorBGR Foreground color in BGR
+ *  \param bgColorBGR Background color in BGR
+ */
+void drawString8x12(u_char col, u_char row, char *string, 
+		   u_int fgColorBGR, u_int bgColorBGR);
+
+/** 8x12 font - this function draws background pixels
+ *  Adapted from RobG's EduKit
+ */
+void drawChar8x12(u_char col, u_char row, char c, 
+		 u_int fgColorBGR, u_int bgColorBGR);
+
+
+/** Draw string at col,row
+ *  Type:
+ *  FONT_SM - small (5x8,) FONT_MD - medium (8x12,) FONT_LG - large (11x16)
+ *  FONT_SM_BKG, FONT_MD_BKG, FONT_LG_BKG - as above, but with background color
+ *  Adapted from RobG's EduKit
+ *
+ *  \param col Column to start drawing string
+ *  \param row Row to start drawing string
+ *  \param string The string
+ *  \param fgColorBGR Foreground color in BGR
+ *  \param bgColorBGR Background color in BGR
+ */
+void drawString11x16(u_char col, u_char row, char *string, 
+		   u_int fgColorBGR, u_int bgColorBGR);
+
+/** 8x12 font - this function draws background pixels
+ *  Adapted from RobG's EduKit
+ */
+void drawChar11x16(u_char col, u_char row, char c, 
+		 u_int fgColorBGR, u_int bgColorBGR);
+
+
 /** Draw rectangle outline
  *  
  *  \param colMin Column start
